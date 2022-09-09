9ff7dbf045d3bb1f7d94446bf27605a1d38cc6e9
Rohit <rohit.t@webkorps.com>
2022-09-09 21:33:59 +0530

    add user data

--- db/migrate/20220909154233_create_users.rb
+++ db/migrate/20220909154233_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :users do |t|4
+      t.string :name
+      t.string :age
+      t.string :city
+      t.string :email
+
 
       t.timestamps
     end
