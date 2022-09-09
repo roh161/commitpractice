5dd5cab01592dc96ff287fdd0365cfc8696e3759
Rohit <rohit.t@webkorps.com>
2022-09-09 21:55:20 +0530

    added cloums in article table

--- db/migrate/20220909160724_create_articles.rb
+++ db/migrate/20220909160724_create_articles.rb
@@ -1,6 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
+      t.string :name
+      t.string :description
+      t.string :text
 
       t.timestamps
     end
